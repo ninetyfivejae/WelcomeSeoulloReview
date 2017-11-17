4e84536aad3dbabc9280c26f026273337ab16080
ninetyfivejae <jh33836782@gmail.com>
2017-11-18 01:48:59 +0900

    SeoulCityAppCompetition WelcomeSeoullo BottomNavigationView review

---
+++ WelcomeSeoulloReview/app/src/main/java/com/example/jaehyukshin/welcomeseoulloreview/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.jaehyukshin.welcomeseoulloreview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        break;
+                    case R.id.action_facility:
+                        break;
+                    case R.id.action_AR:
+                        break;
+                    case R.id.action_route:
+                        break;
+                    case R.id.action_settings:
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
