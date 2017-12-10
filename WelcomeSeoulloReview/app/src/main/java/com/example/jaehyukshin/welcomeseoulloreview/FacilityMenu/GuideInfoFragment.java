2b8cbe172dfd5c98cd077bbb2064c665ec2b91f9
ninetyfivejae <jh33836782@gmail.com>
2017-12-10 23:59:52 +0900

    Tmap View added

--- WelcomeSeoulloReview/app/src/main/java/com/example/jaehyukshin/welcomeseoulloreview/FacilityMenu/GuideInfoFragment.java
+++ WelcomeSeoulloReview/app/src/main/java/com/example/jaehyukshin/welcomeseoulloreview/FacilityMenu/GuideInfoFragment.java
@@ -7,10 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.jaehyukshin.welcomeseoulloreview.R;
+import com.skp.Tmap.TMapView;
 
 public class GuideInfoFragment extends Fragment {
 
     View view;
+
+    RelativeLayout mapView;
+    TMapView tMapView;
+    String apiKey = "1477f615-49bc-39c4-b995-817cd9d4e311";
 
     public GuideInfoFragment(){
 
@@ -19,6 +24,22 @@ public class GuideInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_guide_info, container, false);
+
+        //선언
+        mapView = (RelativeLayout) view.findViewById(R.id.map);
+        tMapView = new TMapView(getActivity());
+
+        //세팅
+        tMapView.setSKPMapApiKey(apiKey); //발급받은 api 키
+        tMapView.setCompassMode(true);
+        tMapView.setIconVisibility(true);
+        tMapView.setZoomLevel(15);
+        tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
+        tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+        tMapView.setTrackingMode(true);
+        tMapView.setSightVisible(true);
+        mapView.addView(tMapView);
+        setContentView(mapView);
 
         return view;
     }
